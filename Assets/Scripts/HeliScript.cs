aebe0ba89957d9653c540b43d4679c190ba5aba8
Emerson Alves <emerson.r.alves.a@gmail.com>
2019-02-03 22:35:43 -0200

    First Commmit

---
+++ Assets/Scripts/HeliScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeliScript : MonoBehaviour {
+
+    public float upForce;
+    public float speed;
+    private Rigidbody2D rigid;
+    private bool isDead = false;
+
+
+    // Use this for initialization
+    void Start () {
+        rigid = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (isDead == false)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rigid.velocity = Vector2.zero;
+                rigid.AddForce(new Vector2(0,upForce));
+            }
+
+            rigid.transform.position = new Vector3(rigid.transform.position.x + (speed / 100), rigid.transform.position.y);
+        }
+	}
+}
---
+++ Assets/Scripts/SpawnObstacle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObstacle : MonoBehaviour {
+
+    public GameObject obstacle;
+    public float distance;
+
+    private float x = 0;
+    private int numberOfObstacles = 0;
+
+    void FixedUpdate()
+    {
+        float y = Random.Range(-2.86f, 2.86f);
+        if (x < 1000 && numberOfObstacles < 10)
+        {
+            Instantiate(obstacle, new Vector3(x * distance, y, 0), Quaternion.identity);
+            x++;
+            numberOfObstacles++;
+        }
+        Debug.Log(x);
+    }
+
+}
