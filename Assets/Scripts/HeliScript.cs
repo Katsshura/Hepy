6916f7e604c3de67b5dbd77643927c77169e3068
emerson <emerson.r.alves.a@gmail.com>
2019-02-09 17:30:10 -0200

    Insertion of comments on code -HeliScript -SpawnObstacle

--- Assets/Scripts/HeliScript.cs
+++ Assets/Scripts/HeliScript.cs
@@ -6,6 +6,12 @@ using UnityEngine.UI;
 
 public class HeliScript : MonoBehaviour {
 
+    /// <summary>
+    /// Here you define all variables you are going to use.
+    /// Public variables are shown on Unity Inspector when you click on the object with this script attached to them.
+    /// Private variables aren't shown on Unity Inspector, if you want a private variable to be shown please see more about [SerializeField]
+    /// </summary>
+
     public float upForce;
     public float speed;
     public Transform camera;
@@ -17,42 +23,79 @@ public class HeliScript : MonoBehaviour {
     private bool isDead = false;
 
 
-    // Use this for initialization
+    /// <summary>
+    /// This method is called when the Object with this script attached is initialized.
+    /// Use it to start variables or define behaviours that you want your code to have.
+    /// </summary>
     void Start () {
+
+        // Obtaining RigidBody2D which is in Object and referencing in to rigid variable.
         rigid = GetComponent<Rigidbody2D>();
+
+        //Initialization of score with 0
         score = 0;
 	}
-	
-	// Update is called once per frame
-	void Update () {
 
+    /// <summary>
+    /// This method is called once per frame during the executation of your app
+    /// Use it to check stats of variables, to make loops, check for inputs and etc
+    /// </summary>
+    void Update () {
+
+        //Checking if player is dead, I only want the code to be executed if he isn't dead.
         if (isDead == false)
         {
+            //Getting Input from Keyboard space for debuging on editor and MouseButton
+            //Ps.: On mobile unity get's touch as mouse button as well
             if (Input.GetKeyDown(KeyCode.Space) ||  Input.GetMouseButtonDown(0))
             {
+                //Setting velocity to 0 and adding a force to it, so it can "jump"
                 rigid.velocity = Vector2.zero;
                 rigid.AddForce(new Vector2(0,upForce));
             }
 
+            //Making the bird deslocate itself on X axis (for the bird, we only need to specify X and Y).
             rigid.transform.position = new Vector3(rigid.transform.position.x + (speed / 100), rigid.transform.position.y);
+
+            //Making Camera follows bird on X axis, we need to specify Z axis as well because we need the camera to be far from the bird.
             camera.position = new Vector3(rigid.transform.position.x, camera.position.y, camera.position.z);
         }
 	}
 
+    /// <summary>
+    /// This method is called every time the bird pass through a trigger collider.
+    /// </summary>
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //When the bird hit's the trigger it means that he passed through an obstacle
+
+        //So increase score
         score++;
+        //Set score on UI
         scoreText.text = score.ToString();
+
+        //Destroy the obstacle after a determinated time.
         Destroy(collision.gameObject.transform.parent.gameObject, 10f);
+
+        //Find and get SpawnObstacle and decrease the number of obstacles.
         FindObjectOfType<SpawnObstacle>().numberOfObstacles--;
     }
 
+    /// <summary>
+    /// This method is called every time the bird collides with a collider.
+    /// </summary>
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        //If collided means that game is over so:
+
+        //Define that player is dead
         isDead = true;
+
+        //Activate Game Over UI
         gameOverScreen.SetActive(true);
     }
 
+    //Method to reload the actual scene.
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
--- Assets/Scripts/SpawnObstacle.cs
+++ Assets/Scripts/SpawnObstacle.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class SpawnObstacle : MonoBehaviour {
 
+    /// <summary>
+    /// Here you define all variables you are going to use.
+    /// Public variables are shown on Unity Inspector when you click on the object with this script attached to them.
+    /// Private variables aren't shown on Unity Inspector, if you want a private variable to be shown please see more about [SerializeField]
+    /// </summary>
+    /// 
     public GameObject obstacle;
     public float distance;
 
@@ -13,10 +19,16 @@ public class SpawnObstacle : MonoBehaviour {
 
     void FixedUpdate()
     {
+        //Get a random value from minimun range and max range on Y axis
         float y = Random.Range(-2.86f, 2.86f);
+
+        //Define max number of obstacles at once
         if (x < 1000 && numberOfObstacles < 10)
         {
+            //Create gameObject on GameScene
             Instantiate(obstacle, new Vector3(x * distance, y, 0), Quaternion.identity);
+
+            //Increase X axis and number of obstacles;
             x++;
             numberOfObstacles++;
         }
